a76b2d4e36297d31afa6e60740ad8d3e7b5fcb9b
ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-06-26 15:45:08 -0300

    cambio de estilos en usuario

--- client/src/features/users/UsersView.jsx
+++ client/src/features/users/UsersView.jsx
@@ -37,50 +37,51 @@ const UsersView = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container mx-auto px-4 py-6 flex flex-col flex-grow">
       <h2
-        className="text-center text-uppercase m-5"
-        style={{ letterSpacing: "5px", fontWeight: "ligher" }}
+        className="text-center flex flex-row justify-between text-2xl font-semibold mb-10 "
+        
       >
         Listado de usuarios del sistema
       </h2>
+      <div className="overflow-x-scroll">
       <table
-        className="table mb-5"
-        style={{ maxWidth: "80%", margin: "auto" }}
+        className="w-full table-auto "
+        // style={{ maxWidth: "80%", margin: "auto" }}
       >
-        <thead>
-          <tr style={{ background: "#006877", color: "white" }}>
-            <th>N</th>
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Numero WA - Sync</th>
-            <th>Usuario</th>            
-            <th>back WA</th>
-            <th>Admin</th>
-            <th>Acción</th>
+        <thead className="bg-green-500 text-white">
+          <tr>
+            <th className="px-4 py-2 text-left">N</th>
+            <th className="px-4 py-2 text-left">Id</th>
+            <th className="px-4 py-2 text-left">Nombre</th>
+            <th className="px-4 py-2 text-left">Numero WA - Sync</th>
+            <th className="px-4 py-2 text-left">Usuario</th>            
+            <th className="px-4 py-2 text-left">back WA</th>
+            <th className="px-4 py-2 text-left">Admin</th>
+            <th className="px-4 py-2 text-left">Acción</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="bg-white  divide-gray-200">
           {users &&
             users.map((user, index) => {
               const { id, name, cellphone, country, username, backwa, isAdmin, vinculated, qrcode } = user;
               return (
-                <tr key={id}>
-                  <th>{index + 1}</th>
-                  <th>{id}</th>
-                  <td>{name}</td>
-                  <td>{cellphone} - {vinculated === true ? "si" : "no"} </td>
-                  <td>{username}</td>
-                  <td>{backwa}</td>
-                  <td>{isAdmin === true ? "si" : "no" }</td>
-                  <td className="d-flex gap-2">
+                <tr key={id} className="hover:bg-gray-50">
+                  <th className="border px-4 py-2">{index + 1}</th>
+                  <th className="border px-4 py-2">{id}</th>
+                  <td className="border px-4 py-2">{name}</td>
+                  <td className="border px-4 py-2">{cellphone} - {vinculated === true ? "si" : "no"} </td>
+                  <td className="border px-4 py-2">{username}</td>
+                  <td className="border px-4 py-2">{backwa}</td>
+                  <td className="border px-4 py-2">{isAdmin === true ? "si" : "no" }</td>
+                  <td className="border px-4 py-2 flex space-x-4 justify-center">
                     <Link to="/edit-user" state={{ id, name, cellphone, country, username, backwa, isAdmin, vinculated, qrcode }}>
-                      <button data-tooltip-id="my-tooltip" data-tooltip-content="Editar Usuario">
+                      <button className="text-blue-500 hover:text-blue-700"  data-tooltip-id="my-tooltip" data-tooltip-content="Editar Usuario">
                         <FaEdit />
                       </button>
                     </Link>
 
-                    <button data-tooltip-id="my-tooltip" data-tooltip-content="Borrar Contacto" onClick={() => handleDelete(id, name)}>
+                    <button className="text-red-500 hover:text-red-700"  data-tooltip-id="my-tooltip" data-tooltip-content="Borrar Contacto" onClick={() => handleDelete(id, name)}>
                       <FaTrashAlt />
                     </button>
                   </td>
@@ -89,6 +90,7 @@ const UsersView = () => {
             })}
         </tbody>
       </table>
+      </div>
       <Tooltip id="my-tooltip" />
     </div>
   );
