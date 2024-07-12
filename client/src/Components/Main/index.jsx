3bb8cf970b908b28e01af6f98a4dde6a23cc4a5e
agustin <agustmontesi@gmail.com>
2024-07-12 12:31:42 -0300

    restore code

--- client/src/Components/Main/index.jsx
+++ client/src/Components/Main/index.jsx
@@ -27,6 +27,7 @@ const Main = () => {
   const login = useSelector((state) => state.usersReducer.login);
   const [vincu, setVincu] = useState("");
   const [isloading, setIsLoading] = useState(true);
+  const [txtVincu, setTxtVincu] = useState("")
 
   async function fetchData() {
     if (login.id) {
@@ -66,8 +67,14 @@ const Main = () => {
   }
 
   useEffect(() => {
-    if (!configs?.length) fetchData();
+    if (configs === null) {
+      dispatch(logOut());
+      navigate("/login");
+      return
+    }
+    if (!configs.length) fetchData();
     const QRobten = login.vinculated;
+    console.log(vincu);
     setVincu(QRobten === false ? t("main.deniedVincMessage") : t("main.successVincMessage"));
     setIsLoading(false);
   }, []);
@@ -81,8 +88,10 @@ const Main = () => {
       <header className="text-center mb-8 flex justify-between items-center mx-10">
         <h1 className="text-3xl font-bold">
           {i18n.language === 'en' ? `${configs?.business}'s Control Panel` : `Panel de control de ${configs?.business}`}
-        </h1>        <div className="flex items-center">
-          <span>{vincu}</span>
+        </h1>
+
+        <div className="flex items-center">
+          <span>{!login.vinculated ? t("main.deniedVincMessage") : t("main.successVincMessage")} </span>
           {login.vinculated ? (
             <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
