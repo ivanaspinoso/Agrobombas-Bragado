efff4764c42fe1177823d3acd28b66308b9b484c
ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-10-30 10:35:52 -0300

    cambio de movimientos por caja

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -182,7 +182,7 @@ const resources = {
         contacts: "Empresa",
         messages: "Productos",
         stack: "Clientes",
-        sent: "Movimientos",
+        sent: "Caja",
         received: "Cuentas Corrientes",
         settings: "Ventas",
         logout: "Salir",
