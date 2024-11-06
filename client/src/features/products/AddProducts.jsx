9657fb490f3dffc0ff114cf120a9ba779785419b
Ivana <sivana361@gmail.com>
2024-11-06 16:09:15 -0300

    ultimos cambios

--- client/src/features/products/AddProducts.jsx
+++ client/src/features/products/AddProducts.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+// import { cateAdd } from "../../app/actions/categories"; // Cambia a la acción correcta si es necesario
+// import { addProduct } from "./ProductsSlice";
+import { createProduct } from "./ProductsSlice";
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import Swal from 'sweetalert2';
-import { createProduct } from "./ProductsSlice";
+import "../../App.css";
 
-const AddProduct = () => {
+const AddProducts = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const login = useSelector((state) => state.usersReducer.login);
 
   const schema = Yup.object().shape({
     name: Yup.string().required("El nombre del producto es requerido"),
-    description: Yup.string().optional(),
-    price: Yup.number().required("El precio es requerido"),
-    units: Yup.string().optional(),
-    minunit: Yup.number().required("La unidad mínima es requerida"),
-    stepunit: Yup.number().required("El paso de unidad es requerido"),
+    description: Yup.string().required("La descripcion del producto es requerido"),
+    price: Yup.string().required("El precio del producto es requerido"),
+    cost: Yup.string().required("El costo del producto es requerido"),
+
   });
 
   return (
@@ -26,29 +29,32 @@ const AddProduct = () => {
       </h2>
       <Formik
         validationSchema={schema}
-        initialValues={{
-          name: "",
-          description: "",
-          price: "",
-          units: "unidad",
-          minunit: 1,
-          stepunit: 1,
-        }}
+        initialValues={{ name: "", description: "", price: "",cost:"" }}
+        
         onSubmit={async (values, { setSubmitting, resetForm }) => {
-          await dispatch(createProduct(values));
-          const success = JSON.parse(localStorage.getItem("productAdded"));
+          const products = {
+            name: values.name,
+            description: values.description,
+            price: values.price,
+            cost: values.cost,
+            userid: login.id
+          };
+          console.log("Enviando producto:", products); // Agrega este log
 
+          await dispatch(createProduct(products)); 
+          const success = JSON.parse(localStorage.getItem("productAdded")); 
+          
           if (success) {
             Swal.fire({
-              title: "Producto agregado",
-              text: "¿Desea seguir agregando productos?",
+              title: "Genial!",
+              text: "Proveedor agregado. ¿Desea seguir agregando?",
               icon: "success",
               showDenyButton: true,
               confirmButtonText: 'Sí',
               denyButtonText: 'No',
             }).then((result) => {
               if (result.isConfirmed) {
-                resetForm();
+                resetForm({ name: "", description: "", price: "",cost:"" });
               } else {
                 navigate("/show-messages");
               }
@@ -56,15 +62,21 @@ const AddProduct = () => {
           } else {
             Swal.fire({
               title: "Error",
-              text: "Hubo un problema al agregar el producto.",
-              icon: "error",
+              text: localStorage.getItem("productAdded"),
+              icon: "error"
             });
           }
           setSubmitting(false);
         }}
       >
         {props => {
-          const { values, errors, handleChange, handleBlur, handleSubmit } = props;
+          const {
+            values,
+            errors,
+            handleChange,
+            handleBlur,
+            handleSubmit
+          } = props;
 
           return (
             <Form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -74,28 +86,29 @@ const AddProduct = () => {
                 </label>
                 <input
                   type="text"
+                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
                   id="name"
                   name="name"
                   value={values.name}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
                 />
                 {errors.name && <p className="text-red-500 text-xs italic">{errors.name}</p>}
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-                  Descripción:
+                  Descripcion:
                 </label>
                 <input
                   type="text"
+                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
                   id="description"
                   name="description"
                   value={values.description}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
                 />
+                {errors.description && <p className="text-red-500 text-xs italic">{errors.description}</p>}
               </div>
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">
@@ -103,14 +116,29 @@ const AddProduct = () => {
                 </label>
                 <input
                   type="number"
+                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
                   id="price"
                   name="price"
                   value={values.price}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
                 />
                 {errors.price && <p className="text-red-500 text-xs italic">{errors.price}</p>}
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="cost">
+                  Costo:
+                </label>
+                <input
+                  type="text"
+                  className="form-input mt-1 block w-full border border-gray-300 rounded px-1"
+                  id="cost"
+                  name="cost"
+                  value={values.cost}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.cost && <p className="text-red-500 text-xs italic">{errors.cost}</p>}
               </div>
               <button
                 type="submit"
@@ -126,4 +154,4 @@ const AddProduct = () => {
   );
 };
 
-export default AddProduct;
+export default AddProducts;
