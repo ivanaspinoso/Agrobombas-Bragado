4b081fde2870edd882595d3f70a21fbd0cb75654
Ivana <sivana361@gmail.com>
2025-01-30 11:45:39 -0300

    division en los encabezados

--- client/src/features/Sales/SalesView.jsx
+++ client/src/features/Sales/SalesView.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteSaleById, fetchAllSales } from "./salesSlice";
 import Swal from "sweetalert2";
-import { FaEdit, FaTrashAlt, FaPrint, FaEye, FaBoxOpen, FaTag, FaDollarSign } from "react-icons/fa";
+import { FaEdit, FaTrashAlt, FaPrint, FaEye, FaBoxOpen, FaDollarSign } from "react-icons/fa";
 import { getAllProducts } from "../../app/actions/products";
 import { fetchAllCashflows } from "../Caja/CashflowSlice";
 
@@ -45,7 +45,7 @@ const SalesView = () => {
   return (
     <div className="container mx-auto px-4 py-5 flex flex-col flex-grow">
       <div className="flex justify-between items-center mb-10">
-      <h2 className="text-xl font-semibold">Ventas</h2>
+        <h2 className="text-xl font-semibold">Ventas</h2>
         <button
           className="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-[#0e6fa5] hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
           onClick={() => navigate("/sales/add")}
@@ -56,36 +56,36 @@ const SalesView = () => {
 
       <div className="overflow-x-scroll">
         <table className="w-full table-auto border border-gray-200">
+          {/* Encabezados de la tabla */}
           <thead className="bg-[#0e6fa5] text-white">
             <tr>
-              <th className="px-4 py-2 text-left">#</th>
-              <th className="px-4 py-2 text-left">Fecha</th>
-              <th className="px-4 py-2 text-left">Cliente</th>
-              <th className="px-4 py-2 text-left">Dirección</th>
-              <th className="px-4 py-2 text-left">Subtotal</th>
-              <th className="px-4 py-2 text-left">Total</th>
-              <th className="px-6 py-2 text-right">Acciones</th> {/* Se alinea a la derecha */}
+              <th className="px-4 py-3 text-center border-r border-gray-300">#</th>
+              <th className="px-4 py-3 text-center border-r border-gray-300">Fecha</th>
+              <th className="px-4 py-3 text-center border-r border-gray-300">Cliente</th>
+              <th className="px-4 py-3 text-center border-r border-gray-300">Dirección</th>
+              <th className="px-4 py-3 text-center border-r border-gray-300">Subtotal</th>
+              <th className="px-4 py-3 text-center border-r border-gray-300">Total</th>
+              <th className="px-6 py-3 text-center">Acciones</th>
             </tr>
           </thead>
-          <tbody className="divide-y divide-gray-200">
+
+          <tbody>
             {filteredSales?.map((sale, index) => (
               <React.Fragment key={sale.id}>
                 {/* Fila de la Venta */}
-                <tr className={`hover:bg-gray-50 ${saleid === sale.id ? "bg-blue-100" : ""}`}>
-                  <td className="px-4 py-2">{index + 1}</td>
-                  <td className="px-4 py-2">{new Date(sale.fecha).toLocaleDateString()}</td>
-                  <td className="px-4 py-2">{sale.customer?.name || "Sin Cliente"}</td>
-                  <td className="px-4 py-2">{sale.customer?.address || "Sin dirección"}</td>
-                  <td className="px-4 py-2 text-right">{parseFloat(sale.subtotal || 0).toFixed(2).replace(".", ",")}</td>
-                  <td className="px-4 py-2 text-right">{parseFloat(sale.total || 0).toFixed(2).replace(".", ",")}</td>
-                  <td className="px-6 py-2 text-right"> {/* Alineado a la derecha */}
-                    <div className="flex justify-end gap-2"> {/* Botones empujados a la derecha */}
+                <tr className="border-b border-gray-300 text-center">
+                  <td className="px-4 py-4">{index + 1}</td>
+                  <td className="px-4 py-4">{new Date(sale.fecha).toLocaleDateString()}</td>
+                  <td className="px-4 py-4">{sale.customer?.name || "Sin Cliente"}</td>
+                  <td className="px-4 py-4">{sale.customer?.address || "Sin dirección"}</td>
+                  <td className="px-4 py-4">{parseFloat(sale.subtotal || 0).toFixed(2).replace(".", ",")}</td>
+                  <td className="px-4 py-4">{parseFloat(sale.total || 0).toFixed(2).replace(".", ",")}</td>
+                  <td className="px-6 py-4 text-center">
+                    <div className="flex justify-center gap-2">
                       <button
                         className={`font-bold py-2 px-4 rounded ${
-                          saleid === sale.id
-                          ? "bg-green-600 text-white hover:bg-green-700"
-                          : "bg-blue-500 hover:bg-blue-700 text-white"
-                      }`}                       
+                          saleid === sale.id ? "bg-green-600 text-white hover:bg-green-700" : "bg-blue-500 hover:bg-blue-700 text-white"
+                        }`}
                         onClick={() => toggleViewLines(sale.id)}
                       >
                         <FaEye />
@@ -112,25 +112,26 @@ const SalesView = () => {
                   </td>
                 </tr>
 
+                {/* Detalle de la venta */}
                 {saleid === sale.id && (
-                  <tr>
-                    <td colSpan="7" className="bg-gray-50 p-2 border border-gray-300">
+                  <tr className="text-center">
+                    <td colSpan="7" className="bg-gray-50 p-4 border-t border-gray-300">
                       <div className="space-y-1">
                         {sale.orderlines.map((item, idx) => (
-                          <div key={idx} className="flex justify-between items-center bg-white px-4 py-2 rounded-md shadow">
+                          <div key={idx} className="flex justify-between items-center bg-white px-4 py-3 rounded-md shadow">
                             <div className="flex items-center gap-2">
-                              <FaBoxOpen className="text-blue-500 " />
+                              <FaBoxOpen className="text-blue-500" />
                               <span className="text-sm">{item.quantity}x</span>
-                              <span className="text-sm">{item.article}</span> 
+                              <span className="text-sm">{item.article}</span>
                               <span className="mr-6 text-sm font-medium">{item.name}</span>
-                              {/* <span>- {item.product}</span> */}
                             </div>
                             <div className="text-right font-regular flex gap-2">
                               <span className="text-green-600 text-sm">
-                                <FaDollarSign className="inline-block " /> {parseFloat(item.price).toFixed(2).replace(".", ",")}
+                                <FaDollarSign className="inline-block" /> {parseFloat(item.price).toFixed(2).replace(".", ",")}
                               </span>
                               <span className="text-sm font-medium">
-                              <FaDollarSign className="inline-block " /> {parseFloat(item.subtotal).toFixed(2).replace(".", ",")}</span>
+                                <FaDollarSign className="inline-block" /> {parseFloat(item.subtotal).toFixed(2).replace(".", ",")}
+                              </span>
                             </div>
                           </div>
                         ))}
